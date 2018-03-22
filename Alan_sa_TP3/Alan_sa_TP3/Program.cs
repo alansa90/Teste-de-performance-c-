84052ac5dcacc1e01daf7068d756463377f8e659
Alan Pereira de Sá <alkinsret@gmail.com>
2018-03-21 21:20:57 -0300

    Add files via upload

---
+++ Alan_sa_TP3/Alan_sa_TP3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alan_sa_TP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string var;
+
+            Metodos.CarregarMenuPrincipal();
+            var = Console.ReadLine();
+            Metodos.MenuOpcoes(new List<Lista>(), var
+                );
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TP2/TP2/TP2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string var;
+
+            Metodos.CarregarMenuPrincipal();
+            var = Console.ReadLine();
+            Metodos.MenuOpcoes(new List<Lista>(), var);
+            Console.ReadKey();
+
+        }
+    }
+}
